315f7e8f7e210fe2b1f750c2c31ed3263cc74afc
Hariharan R <hariharan.r@isteer.com>
2024-01-24 21:04:03 +0530

    Sample

---
+++ Demo.js
@@ -0,0 +1,24 @@
+function varScoping() {
+  var x = 1;
+
+  if (true) {
+    var x = 20;
+    console.log(x);
+  }
+
+  console.log(x);
+}
+
+function letScoping() {
+  //let x = 100;
+
+  if (true) {
+    let x = 2;
+    console.log(x);
+  }
+
+  console.log(x);
+}
+
+letScoping();
+//varScoping();
